310277f1f8dc456f8e82df63a4e28a063e1e6aa6
Gary LaMountain <gary@Garys-Laptop.attlocal.net>
2024-11-27 07:36:27 -0600

    pagination works

--- src/components/ReviewApp.tsx
+++ src/components/ReviewApp.tsx
@@ -1,27 +1,68 @@
 import StarRating from "@/components/StarRating";
-import React from "react";
+import React, { useState } from "react";
 import ReviewList from "./ReviewList";
 import Review from "@/data/Review";
 import useFetchReviewData from "@/data/useFetchReviewData";
 
 const ReviewApp = () => {
-  const { reviewData, setReviewData, error } = useFetchReviewData();
-  
+  const [page, setPage] = useState(1);
+  const { reviewData, setReviewData, error } = useFetchReviewData(page);
+
   const submitReview = (newReview: Review) => {
-    // typically this would be a post call to the API
+    // Typically this would be a POST call to the API
     const updatedReviewData = [newReview, ...reviewData];
     setReviewData(updatedReviewData);
-  }
+  };
+
+  const handlePrevious = () => {
+    if(page > 1) setPage(page - 1);
+  };
+
+  const handleNext = () => {
+    if(page !== 3) setPage(page + 1);
+  };
 
   return (
     <div>
-      <StarRating submitReview={submitReview} numOfReviews={reviewData.length}/> 
-      {error && 
+      <StarRating submitReview={submitReview} numOfReviews={reviewData.length} />
+      {error && (
         <div className="p-4 mt-4 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
           <span className="font-medium">Failed to load reviews: </span>{error.message}
         </div>
-      }     
-      <ReviewList reviews={reviewData}/>
+      )}
+      <ReviewList reviews={reviewData} />
+      <div className="flex justify-center items-center mt-4">
+        <div
+          onClick={handlePrevious}
+          className={`relative inline-flex items-center rounded-l-md px-2 py-2 ${
+            page > 1 ? "text-gray-400 hover:bg-gray-50 cursor-pointer" : "text-gray-300 cursor-not-allowed"
+          } ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}
+        >
+          <span className="sr-only">Previous</span>
+          <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+            <path
+              fillRule="evenodd"
+              d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div
+          onClick={handleNext}
+          className={`relative inline-flex items-center rounded-r-md px-2 py-2 ${
+            page !== 3 ? "text-gray-400 hover:bg-gray-50 cursor-pointer" : "text-gray-300 cursor-not-allowed"
+          } ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}
+        >
+          <span className="sr-only">Next</span>
+          <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+            <path
+              fillRule="evenodd"
+              d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ReviewList.tsx
+++ src/components/ReviewList.tsx
@@ -11,18 +11,6 @@ const ReviewList = ({ reviews }: ListProps) => {
 
   return (
     <ul className="list-disc">
-        <div className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 cursor-pointer">
-          <span className="sr-only">Previous</span>
-          <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-            <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
-          </svg>
-        </div>
-        <div className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 cursor-pointer">
-          <span className="sr-only">Next</span>
-          <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-            <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-          </svg>
-        </div>
         {reviews.map(review => {
             return (
                 <div key={review.id}>
